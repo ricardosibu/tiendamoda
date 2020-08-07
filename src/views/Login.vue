db25fea5c7bf182a834b29dbf2f43d0dbd044a6c
rsibu <ricardo.sibu@gmail.com>
2020-08-07 09:00:24 -0400

    login done

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,31 +1,26 @@
 <template>
-  <div class="home">
-    <div
-      class="flex content-center w-screen max-h-full sm:w-screen xs:w-screen mx-auto shadow bg-cover py-12 bg-top"
-      style="background-image: url(https://mitienda.moda/img/login-desktop.0a0cacb5.jpg)"
-    >
-      <div class="flex flex-col justify-center w-64 h-auto">
-        <div class="w-48 h-32 px-2 py-4">
-          <img src="https://mitienda.moda/img/logo.c752bf8f.png" alt="" />
-        </div>
-        <div class="">
-          <label class="font-sans text-lg text-white" for="number"
-            >Ingrese el numero de telefono de tu asesora independiente</label
-          >
-          <input
-            type="text"
-            class="w-48 h-12 px-2 py-4 bg-transparent border-solid border-2 rounded border-white"
-            placeholder="Telefono"
-          />
-          <button
-            class="w-48 h-12 bg-red-300 :hover:bg-red-500"
-            @click="login()"
-          >
-            Ingresar
-          </button>
-        </div>
+  <div
+    class="h-screen max-w-screen bg-no-repeat bg-fixed"
+    style="background-image: url(https://mitienda.moda/img/login-desktop.0a0cacb5.jpg)"
+  >
+    <div class="flex items-center flex-col w-full">
+      <div class="w-56 h-32 px-2 py-4 mt-32">
+        <img src="https://mitienda.moda/img/logo.c752bf8f.png" alt="" />
       </div>
-      <div></div>
+      <div class="w-1/4 mt-10 text-center">
+        <label
+          class="font-sans text-xl text-white text-center font-semibold"
+          for="number"
+          >Ingrese el número de telefono de tu asesora independiente</label
+        >
+        <input
+          type="text"
+          class="w-full h-12 mt-12 px-2 py-4 bg-transparent border-solid border-2 rounded border-white text-4xl text-white font-semibold"
+          placeholder="Telefono"
+          v-model="phoneNumber"
+          @keyup.enter="login()"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -35,11 +30,25 @@ export default {
   name: "Login",
   components: {},
   data() {
-    return {};
+    return {
+      phoneNumber: ""
+    };
+  },
+  created() {
+    this.paswordLocalStorage();
   },
   methods: {
     login() {
-      this.$router.push("/categorias");
+      let phoneAccess = localStorage.getItem("password");
+      if (this.phoneNumber == phoneAccess) {
+        this.$router.push("/categorias");
+      } else {
+        console.log("Acceso no autorizado");
+      }
+    },
+    paswordLocalStorage() {
+      const phoneLogin = 7865470213;
+      localStorage.setItem("password", phoneLogin);
     }
   }
 };
